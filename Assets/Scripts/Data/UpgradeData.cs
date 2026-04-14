43f2c5f516d938c99d58df3fdcecf6a42f48913e
Houssem <ahmedhoussemayachi@gmail.com>
2026-04-14 14:50:40 +0200

    small changes

--- Assets/Scripts/Data/UpgradeData.cs
+++ Assets/Scripts/Data/UpgradeData.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 
 /// <summary>
-/// Définit les types d'upgrades disponibles
+/// Definit les types d'upgrades disponibles
 /// </summary>
 public enum UpgradeType
 {
@@ -16,7 +16,7 @@ public enum UpgradeType
 }
 
 /// <summary>
-/// Classe contenant les données d'une upgrade
+/// Classe contenant les donnees d'une upgrade
 /// </summary>
 public class UpgradeData
 {
@@ -33,7 +33,7 @@ public class UpgradeData
 }
 
 /// <summary>
-/// Génère automatiquement les upgrades basées sur les UpgradeType
+/// Genere automatiquement les upgrades basees sur les UpgradeType
 /// </summary>
 public static class UpgradeFactory
 {
@@ -43,12 +43,12 @@ public static class UpgradeFactory
     
     private static readonly Dictionary<UpgradeType, UpgradeConfig> upgradeConfigs = new Dictionary<UpgradeType, UpgradeConfig>
     {
-        { UpgradeType.WeaponDamage, new UpgradeConfig("Lame Affûtée",   "Dégâts +{0}%",             15f) },
-        { UpgradeType.MaxHealth,    new UpgradeConfig("Vitalité",       "HP max +{0}",              25f) },
-        { UpgradeType.Speed,        new UpgradeConfig("Célérité",       "Vitesse +{0}%",            10f) },
-        { UpgradeType.Armor,        new UpgradeConfig("Peau de Fer",    "Dégâts reçus -{0}%",       10f) },
-        { UpgradeType.HealthRegen,  new UpgradeConfig("Second Souffle", "Récupère {0} HP",          20f) },
-        { UpgradeType.AttackSpeed,  new UpgradeConfig("Frénésie",       "Vitesse d'attaque +{0}%",  15f) }
+        { UpgradeType.WeaponDamage, new UpgradeConfig("Lame Affetee",   "Degats +{0}%",             15f) },
+        { UpgradeType.MaxHealth,    new UpgradeConfig("Vitalite",       "HP max +{0}",              25f) },
+        { UpgradeType.Speed,        new UpgradeConfig("Celerite",       "Vitesse +{0}%",            10f) },
+        { UpgradeType.Armor,        new UpgradeConfig("Peau de Fer",    "Degats recus -{0}%",       10f) },
+        { UpgradeType.HealthRegen,  new UpgradeConfig("Second Souffle", "Recupere {0} HP",          20f) },
+        { UpgradeType.AttackSpeed,  new UpgradeConfig("Frenesie",       "Vitesse d'attaque +{0}%",  15f) }
     };
 
     private class UpgradeConfig
@@ -103,42 +103,30 @@ public static class UpgradeFactory
 
     private static void ApplyUpgradeEffect(UpgradeType type, float value)
     {
-        if (PlayerStats.Instance == null)
-        {
-            Debug.LogError("[UpgradeFactory] PlayerStats.Instance est NULL !");
-            return;
-        }
-
         switch (type)
         {
             case UpgradeType.WeaponDamage:
                 PlayerStats.Instance.IncreaseWeaponDamage(value);
-                Debug.Log($"?? Dégâts +{value}%");
                 break;
 
             case UpgradeType.MaxHealth:
                 PlayerStats.Instance.IncreaseMaxHealth((int)value);
-                Debug.Log($"?? HP max +{value}");
                 break;
 
             case UpgradeType.Speed:
                 PlayerStats.Instance.IncreaseSpeed(value);
-                Debug.Log($"? Vitesse +{value}%");
                 break;
 
             case UpgradeType.Armor:
                 PlayerStats.Instance.IncreaseArmor(value);
-                Debug.Log($"??? Armure +{value}%");
                 break;
 
             case UpgradeType.HealthRegen:
                 PlayerStats.Instance.Heal((int)value);
-                Debug.Log($"?? Soigné de {value} HP");
                 break;
 
             case UpgradeType.AttackSpeed:
                 PlayerStats.Instance.IncreaseAttackSpeed(value);
-                Debug.Log($"?? Vitesse d'attaque +{value}%");
                 break;
         }
     }
@@ -147,7 +135,7 @@ public static class UpgradeFactory
     {
         List<UpgradeData> allUpgrades = GenerateAllUpgrades();
 
-        // Mélanger (Fisher-Yates shuffle)
+        // Melanger (Fisher-Yates shuffle)
         for (int i = allUpgrades.Count - 1; i > 0; i--)
         {
             int j = UnityEngine.Random.Range(0, i + 1);
